7bbd174338f64f21fdcce47c1984caa1338c1965
King8111 <mshabeeb4@gmail.com>
2021-11-04 19:49:04 +0530

    final

--- calcscript.js
+++ calcscript.js
@@ -1,27 +1,29 @@
-function checkloveper(){
+function checkloveper() {
 
     var name = document.getElementById('name').value;
     var lname = document.getElementById('lname').value;
-  
-    if(name == ""){
-     alert('Please enter your name');
-    }else if (name.length <=2 ) {
-     alert('Min lenght is 3')
-    }else if(!isNaN(name)){
-     alert('Numbers are not allowed');
+
+    if (name == "") {
+        alert('Please enter your name');
+    } else if (name.length <= 2) {
+        alert('Min lenght is 3')
+    } else if (!isNaN(name)) {
+        alert('Numbers are not allowed');
     }
-  
-    else if(lname == ""){
-     alert('Please enter your love name');
-    }else if (lname.length <=2 ) {
-     alert('Min lenght is 3')
-    }else if(!isNaN(lname)){
-     alert('Numbers are not allowed');
+
+    else if (lname == "") {
+        alert('Please enter your Partner name');
+    } else if (lname.length <= 2) {
+        alert('Min lenght is 3')
+    } else if (!isNaN(lname)) {
+        alert('Numbers are not allowed');
     }
-    else{
-    var lovdata = Math.random() * 100;
-    lovdata = Math.floor(lovdata);
-    document.getElementById('lovevalue').value = lovdata + "%"; 
+    else {
+        var lovdata = Math.random() * 100;
+        lovdata = Math.floor(lovdata);
+        document.getElementById('lovevalue').value = lovdata + "%";
     }
-    
-   }
+    document.getElementById('name').value='';
+    document.getElementById('lname').value='';
+
+}
